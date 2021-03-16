f7d2e6a53415dba02628f862afac31e8ead29183
mzw86020424 <mzw86020424@gmail.com>
2021-03-16 17:06:23 +0900

    line_food.controllerのindexアクションを定義

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -3,6 +3,20 @@ module Api
     class LineFoodsController < ApplicationController
       before_action :set_food, only: %i[create]
 
+      def index
+        line_foods = LineFood.active
+        if line_foods.exists?
+          render json: {
+            line_food_ids: line_foods.map { |line_food| line_food.id },
+            restaurant: line_foods[0].restaurant,
+            count: line_foods.sum { |line_food| line_food[:count] },
+            amount: line_foods.sum { |line_food| line_food.total_amount },
+          }, status: :ok
+        else
+          render json: {}, status: :no_content
+        end
+      end
+
       def create
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
           return render json: {
@@ -29,6 +43,7 @@ module Api
       end
 
       def set_line_food(ordered_food)
+        # Railsガイド belongs_to関連付けの詳細 https://railsguides.jp/association_basics.html#belongs-to%E3%81%A7%E8%BF%BD%E5%8A%A0%E3%81%95%E3%82%8C%E3%82%8B%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89
         # 同じfoodに紐づいているline_foodがあるかどうか
         if ordered_food.line_food.present? # すでに存在するline_foodに足す
           @line_food = ordered_food.line_food
