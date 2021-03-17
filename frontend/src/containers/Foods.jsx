131f1586c84efa4ad202ee58a4d867b6533f24a9
mzw86020424 <mzw86020424@gmail.com>
2021-03-17 09:58:37 +0900

    フード一覧のAPIデータをreducerを通して画面に表示する

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,22 +1,47 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
+
+// reducers
+import {
+  foodsReducer,
+  foodsActionTypes,
+  initialState as foodsInitialState,
+} from '../reducers/foods';
 
 // apis
 import { fetchFoods } from '../apis/foods';
 
+// constants
+import { REQUEST_STATE } from '../constants';
+
 export const Foods = ({ match }) => { // match: react-router使用時に使うprops
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
   useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
     fetchFoods(match.params.restaurantsId)
-      .then((data) =>
-        console.log(data)
-    )
+      .then((data) => {
+        dispatch({
+          type: foodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      })
   }, [])
 
   return (
     <Fragment>
-      フード一覧
-      <p>
-        restaurantsIdは {match.params.restaurantsId} です
-      </p>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>LOADING...</p>
+        </Fragment>
+          :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          ) 
+      }
     </Fragment>
   )
 }
